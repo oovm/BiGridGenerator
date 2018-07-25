f289b6d55faadfda6b044c1e041288b7dfa44691
GalAster <galaster@foxmail.com>
2018-07-25 15:44:25 +0800

    项目::ASCII 画, 改进 2

--- Working/Art/ASCII.m
+++ Working/Art/ASCII.m
@@ -1,22 +1,7 @@
 Options[ASCIIfy] = {FontFamily -> "Inconsolata", ColorNegate -> False};
-ASCIIfy[graphicchoice_, charchoice_, cbasic_, grainchoice_, coloredchoice_, OptionsPattern[]] := Module[
-
-(*choose graphic: image or gif*)
-
-(*choose alphabet here, "Auto" uses your location *)
-
-(*choose grain, "Auto" uses size of characters*)
-
-(*choose between colored or black characters*)
-	
+ASCIIfy[img_, charchoice_, cbasic_, grainchoice_, coloredchoice_, OptionsPattern[]] := Module[
 	{},
-
-(*Is this a gif? if not then make it so*)
-	If[ListQ[graphicchoice],
-		graphic = graphicchoice,
-		graphic = List[graphicchoice]];
-	
-	(*Chars used in output that are not part of the alphabets*)
+(*Chars used in output that are not part of the alphabets*)
 	If[
 		cbasic,
 		charsbasic = {"$", "&", "'", ",", ".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "?"},
@@ -52,62 +37,42 @@ ASCIIfy[graphicchoice_, charchoice_, cbasic_, grainchoice_, coloredchoice_, Opti
 	(*dark images will not be well represented by chars, as they have a white background, so before applying the program the image brightness is scales up accordingly*)
 	bleach[im_] := im * (1 - Min[charsmean]) + Min[charsmean];
 	
-	Monitor[
-	
 	(*creates a table of rasterized columns of strings that resemble frames of a gif*)
-		giftable = Table[
-		(*Should it color negate?*)
-			If[OptionValue[ColorNegate] === True,
-				graphicframe = ColorNegate[graphic[[frame]]],
-				graphicframe = graphic[[frame]]
-			];
-			
-			(*creates a partitioned black and white, bleached image*)
-			pic = ImagePartition[ImageApply[bleach, {ColorConvert[graphicframe, "Grayscale"]}], {charwidth, charheight}];
-			
-			(*creates a partitioned colored image if coloredchoice is true*)
-			If[coloredchoice, picwc = ImagePartition[graphicframe, {charwidth, charheight}];, ""];
-			
-			(*calculated the dimensions of the partitioned image*)
-			{height, width} = Dimensions[pic];
-			
-			(*calculates the average brightness for each piece of the image*)
-			picmeans = Table[ImageMeasurements[pic[[y, x]], "Mean"], {x, 1, width}, {y, 1, height}];
-			
-			(*names a function that calculates the differences between a piece of the image and all chars*)
-			diffs[rownumber_, placeinrow_] := Table[charsmean[[a]] - picmeans[[rownumber, placeinrow]], {a, 1, Length[chars]}]^2;
-			
-			(*picks the character whoose difference is the smallest*)
-			best[x_, y_] := chars[[Position[diffs[x, y], Min[diffs[x, y]]][[1, 1]]]];
-			
-			(*creates a column of rows of colored best fitting letters, colored or black*)
-			If[coloredchoice,
-				charpic = TableForm[Table[Style[best[testx, testy], FontColor -> RGBColor[ImageMeasurements[picwc[[testy, testx]], "Mean"]]], {testy, 1, height}, {testx, 1, width}], TableSpacing -> {0, 0}],
-				charpic = TableForm[Table[Style[best[testx, testy], FontColor -> Black], {testy, 1, height}, {testx, 1, width}], TableSpacing -> {0, 0}]
-			];
-			
-			(*rasterizes the column of strings*)
-			If[
-				OptionValue[ColorNegate] == True,
-				ColorNegate[Rasterize[Style[TableForm[Map[Pane[#, {truecharwidth, truecharheight}, Alignment -> Center]&, charpic, {3}], TableSpacing -> {0, 1}], FontFamily -> OptionValue[FontFamily]]]],
-				Rasterize[Style[TableForm[Map[Pane[#, {truecharwidth, truecharheight}, Alignment -> Center]&, charpic, {3}], TableSpacing -> {0, 1}], FontFamily -> OptionValue[FontFamily]]]
-			],
-		
-		(*repetition setting: as many times as the gif is long*)
-			{frame, 1, Length[graphic]}
-		],
 	
-	(*monitor setting: shows progress and total number of frames*)
-		{(testy - 1) / height * 1. + (frame - 1), Length[graphic]}];
 	
-	(*exports the table of images into a gif or jpg*)
-	If[ListQ[graphicchoice],
-		Export["giftable.gif", giftable, "AnimationRepetitions" -> \[Infinity]],
-		Export["giftable.jpg", giftable[[1]]]
+	(*Should it color negate?*)
+	graphicframe = If[OptionValue[ColorNegate] === True, ColorNegate[img], img];
+	
+	(*creates a partitioned black and white, bleached image*)
+	pic = ImagePartition[ImageApply[bleach, {ColorConvert[graphicframe, "Grayscale"]}], {charwidth, charheight}];
+	
+	(*creates a partitioned colored image if coloredchoice is true*)
+	If[coloredchoice, picwc = ImagePartition[graphicframe, {charwidth, charheight}];, ""];
+	
+	(*calculated the dimensions of the partitioned image*)
+	{height, width} = Dimensions[pic];
+	
+	(*calculates the average brightness for each piece of the image*)
+	picmeans = Table[ImageMeasurements[pic[[y, x]], "Mean"], {x, 1, width}, {y, 1, height}];
+	
+	(*names a function that calculates the differences between a piece of the image and all chars*)
+	diffs[rownumber_, placeinrow_] := Table[charsmean[[a]] - picmeans[[rownumber, placeinrow]], {a, 1, Length[chars]}]^2;
+	
+	(*picks the character whoose difference is the smallest*)
+	best[x_, y_] := chars[[Position[diffs[x, y], Min[diffs[x, y]]][[1, 1]]]];
+	
+	(*creates a column of rows of colored best fitting letters, colored or black*)
+	If[coloredchoice,
+		charpic = TableForm[Table[Style[best[testx, testy], FontColor -> RGBColor[ImageMeasurements[picwc[[testy, testx]], "Mean"]]], {testy, 1, height}, {testx, 1, width}], TableSpacing -> {0, 0}],
+		charpic = TableForm[Table[Style[best[testx, testy], FontColor -> Black], {testy, 1, height}, {testx, 1, width}], TableSpacing -> {0, 0}]
 	];
 	
-	(*opens the gif*)
-	If[ListQ[graphicchoice],
-		SystemOpen["giftable.gif"],
-		SystemOpen["giftable.jpg"]]
+	(*rasterizes the column of strings*)
+	If[
+		OptionValue[ColorNegate] == True,
+		ColorNegate[Rasterize[Style[TableForm[Map[Pane[#, {truecharwidth, truecharheight}, Alignment -> Center]&, charpic, {3}], TableSpacing -> {0, 1}], FontFamily -> OptionValue[FontFamily]]]],
+		Rasterize[Style[TableForm[Map[Pane[#, {truecharwidth, truecharheight}, Alignment -> Center]&, charpic, {3}], TableSpacing -> {0, 1}], FontFamily -> OptionValue[FontFamily]]]
+	]
+
+(*repetition setting: as many times as the gif is long*)
 ]
