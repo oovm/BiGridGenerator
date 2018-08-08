6fe1835c807e164a81a3e0ae561019832971ece2
GalAster <galaster@foxmail.com>
2018-08-08 20:55:25 +0800

    代码::数独求解

---
+++ Working/Game/数独.m
@@ -0,0 +1,12 @@
+problemStr="800 000 000 003 600 000 070 090 200 050 007 000 000 045 700 000 100 030 001 000 068 008 500 010 090 000 400";
+problem=Partition[ToExpression@Characters@StringDelete[problemStr," "],9];
+blocks=Table[3 Quotient[i,3]+Quotient[j,3],{i,0,8},{j,0,8}];
+rows=Table[i,{i,0,8},{j,0,8}];
+cols=Table[j,{i,0,8},{j,0,8}];
+constrain[blk_]:=Join@@Table[Outer[Plus,Range[9],(Position[blk,k]-1).{81,9}],{k,Min[blk],Max[blk]}];
+numberConstraints=Table[Range[9]+9 m,{m,0,80}];
+basicConstraints=Join@@Thread[Unevaluated@constrain[{blocks,rows,cols}]];
+problemConstraints=Join@@Table[List/@(Append[k]/@(Position[problem,k]-1)).{81,9,1},{k,1,9}];
+allConstraints=(Normal@SparseArray[Thread[#->1],729]&)/@Join[basicConstraints,numberConstraints,problemConstraints];
+lpResult=Quiet@LinearProgramming[ConstantArray[0,729],allConstraints,ConstantArray[{1,0},Length@allConstraints],ConstantArray[{0,1},729],Integers];
+Grid[Partition[FirstPosition[#,1][[1]]&/@Partition[lpResult,9],9],Frame->All]
